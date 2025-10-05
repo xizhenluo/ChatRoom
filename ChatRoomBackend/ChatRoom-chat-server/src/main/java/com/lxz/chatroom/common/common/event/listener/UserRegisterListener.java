0175a84a59686c3fd024091dea6da201e0ba7a5b
Xizhen Luo <xizhenluowork@gmail.com>
2025-10-05 21:44:13 +0100

    revise bugs in idempotent; allocat items when registeration using spring event

---
+++ ChatRoomBackend/ChatRoom-chat-server/src/main/java/com/lxz/chatroom/common/common/event/listener/UserRegisterListener.java
@@ -0,0 +1,6 @@
+package com.lxz.chatroom.common.common.event.listener;/**
+  * @Description
+  * @author <a href="https://github.com/xizhenluo">LuoXizhen</a>
+  * @date 2025/10/5
+ */public class UserRegisterListener {
+}
